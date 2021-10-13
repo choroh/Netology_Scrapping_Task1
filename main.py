239d0338730fa075218cd24be7b34dc2f72756c4
Choroh <allmy66@mail.ru>
2021-10-13 12:56:06 +0400

    Добавил поиск из списка ключевых слов с любым содержимым и различный регистр

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import requests
 import bs4
 
 KEYWORDS = ['дизайн', 'фото', 'web', 'python']
-
 #  Будем искать статьи с этими ключевыми словами
 
 url = 'https://habr.com/ru/all'
@@ -35,15 +34,20 @@ for article in articles:
     #  Перебираем блок по статьям
     preview = article.find("div", class_="tm-article-body tm-article-snippet__lead").text.strip()
     #  Получаем превью статей
-    if KEYWORDS[0] in preview or KEYWORDS[1] in preview or KEYWORDS[2] in preview or KEYWORDS[3] in preview:
-        title = article.find('h2').text.strip()
-        href = article.find(class_= 'tm-article-snippet__title-link').attrs['href']
-        link = domain + href
-        #  Получили заголовок и ссылку
-        #  data = article.find(class_="tm-article-snippet__datetime-published").text
-        #  вывод даты в виде "сегодня в 15:00"
-        data = article.find("span", class_="tm-article-snippet__datetime-published").find("time").get("title")
-        #  Находим запись даты, выделяем тег time и получаем от туда дату и время из title
-        print(f'{data} - {title} - {link}')
+    for key in KEYWORDS:
+        #  Вариант поиска различных ключевых слов из списка
+        #if KEYWORDS[0] in preview or KEYWORDS[1] in preview or KEYWORDS[2] in preview or KEYWORDS[3] in preview:
+        #  Вариант поиска фиксированных ключевых слов из задания
+        if key.title() in preview or key.lower() in preview or key.upper() in preview:
+            title = article.find('h2').text.strip()
+            href = article.find(class_='tm-article-snippet__title-link').attrs['href']
+            link = domain + href
+            #  Получили заголовок и ссылку
+            #  data = article.find(class_="tm-article-snippet__datetime-published").text
+            #  вывод даты в виде "сегодня в 15:00"
+            data = article.find("span", class_="tm-article-snippet__datetime-published").find("time").get("title")
+            #  Находим запись даты, выделяем тег time и получаем от туда дату и время из title
+            print(f'{data} - {title} - {link}. ключевое слово {key}')
+            break
 
 
